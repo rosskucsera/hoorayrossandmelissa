a3b179e9e365ae4c89c7adec4be57a9ab0031cc5
Ross Kucsera <ross.kucsera@peoplematter.com>
2013-10-11 21:26:03 -0400

    update to June template

---
+++ index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<meta name="description" content=" "><!-- <<<<   Add a brief decription of your app -->
+
+<meta name="copyright" content="<?php echo date("Y") ?>, App Name" />
+
+<title>Ross and Melissa are getting married</title>
+
+<!----  User-Scalable = NO  ---->
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+
+<!----  CSS  ---->
+<link href="site/css/styles.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+
+<body onload="MM_preloadImages('site/images/header_background2.png')">
+
+
+
+
+<!----  Header  ---->
+
+<div class="header_wrapper">
+
+<div class="header">
+
+<div class="inner_wrapper">
+
+<div class="header_left">
+
+<div class="top_header">
+<h1>Ross and Melissa <br><strong>March 28, 2014</strong><br><strong>Charleston, SC</strong></h1>
+
+</div>
+
+<div class="mid_header">
+
+<p>We are getting married and would love for you to join us. We'll be updating this site with info about the big day. So stay tuned.</p>
+
+
+</div>
+</div>
+
+<div class="header_right">
+
+<div class="iphone_one"></div>
+<div class="iphone_two"></div>
+</div>
+
+</div>
+
+</div>
+
+<div class="divide"></div>
+
+</div>
+
+
+
+
+<!----  Section one  ---->
+
+<div class="section_one">
+
+<div class="waypoint"></div>
+
+<div class="inner_section">
+
+<div class="iphone_section_one"></div>
+
+<div class="section_text_wrapper">
+<h2>Save <strong>the date</strong></h2>
+<p>We won't forget the date and we hope you don't either. Just in case, download a calendar reminder for March 28th </p>
+</div>
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+<!----  Section two  ---->
+
+<div class="section_two">
+
+<div class="waypoint"></div>
+
+<div class="inner_section">
+
+<div class="section_text_wrapper">
+
+<h2>Live <strong>Updates</strong></h2>
+<p>Mauris fringilla neque eu turpis dictum non vestibulum odio rutrum. Aenean blandit mattis urna, quis eleifend diam semper ac.</p>
+</div>
+<div class="iphone_section_two"></div>
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+<!----  Section three  ---->
+
+<div class="section_three">
+
+<div class="waypoint"></div>
+
+<div class="inner_section">
+
+<div class="iphone_section_three"></div>
+
+<div class="section_text_wrapper">
+
+<h2>New <strong>Auto-Save</strong></h2>
+<p>Mauris fringilla neque eu turpis dictum non vestibulum odio rutrum. Aenean blandit mattis urna, quis eleifend diam semper ac.</p>
+</div>
+
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+<!----  Section - the team  ---->
+
+<div class="section_team">
+<div class="inner_wrapper">
+<div class="team_title">
+<h3>Accomodation</h3>
+<p>We've let a few hotels in the area know there's a big party going on. Book with them and be sure to ask for the Kucsera-Whitson block.</p>
+</div>
+
+
+
+
+
+<!----  Team member one  ---->
+
+<div class="team_member_wrapper">
+<div class="team_user_icon team_member_one"></div>
+<div class="team_member_info">
+<h4>Team Member</h4>
+<p>Designer</p>
+<a href="#" class="team_social_icon_twitter" title="Follow on Twitter"></a>
+<a href="#" class="team_social_icon_dribbble" title="Follow on Dribbble"></a>
+</div>
+</div>
+
+
+
+
+
+
+<!----  Team member two  ---->
+
+<div class="team_member_wrapper">
+<div class="team_user_icon team_member_two"></div>
+<div class="team_member_info">
+<h4>Hampton Inn Charleston/Mt. Pleasant<br>Patriots Point</h4>
+<p>Developer</p>
+<a href="#" class="team_social_icon_twitter" title="Follow on Twitter"></a>
+<a href="#" class="team_social_icon_dribbble" title="Follow on Dribbble"></a>
+</div>
+</div>
+
+
+
+
+
+<!----  Team member three  ---->
+
+<div class="team_member_wrapper">
+<div class="team_user_icon team_member_three"></div>
+<div class="team_member_info">
+<h4>Team Member</h4>
+<p>Designer</p>
+<a href="#" class="team_social_icon_twitter" title="Follow on Twitter"></a>
+<a href="#" class="team_social_icon_dribbble" title="Follow on Dribbble"></a>
+</div>
+</div>
+
+
+
+
+</div>
+</div>
+
+
+
+
+
+
+<!----  Section features  ---->
+
+
+<div class="section_features">
+<div class="inner_wrapper">
+
+<h3>These people said good things about us....</h3>
+
+<a class="feature_logo_one" href="#"></a>
+<a class="feature_logo_two" href="#"></a>
+<a class="feature_logo_three" href="#"></a>
+<a class="feature_logo_four" href="#"></a>
+
+</div>
+
+<div class="divide"></div>
+</div>
+
+
+
+
+<!----  Footer  ---->
+
+
+<div class="footer">
+<div class="inner_wrapper">
+
+<div class="footer_left"><p>AppName</p>© <?php echo date("Y") ?></div>
+
+<div class="footer_right">
+<a href="#" class="twitter_icon" title="Follow us on Twitter"></a>
+<a href="#" class="facebook_icon" title="Find us on Facebook"></a>
+<a href="#" class="appstore_icon" title="Find us on the App Store"></a>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+<!----  Scripts ---->
+<script src="site/js/jquery-1.9.1.min.js"></script>
+<script src="site/js/waypoints.min.js"></script>
+<script src="site/js/site.js"></script>
+
+
+<!----  Preload Images  ---->
+<script>
+function MM_preloadImages() { //v3.0
+var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+</script>
+
+
+</body>
+</html>
