ab136f4409f51bff987172be61374109f09852f1
Ross Kucsera <ross.kucsera@peoplematter.com>
2014-02-12 23:48:11 -0500

    waypoint offsets

--- site/js/site.js
+++ site/js/site.js
@@ -52,23 +52,23 @@ $(".rsvp, .gifts, .when_where, .about_us, .where_to_stay, .section_team, .sectio
 <!----  Waypoints (scroll animations)  ---->
 $('.rsvp .waypoint').waypoint(function(direction) {
 $(".rsvp .inner_section").show().addClass('animated fadeInLeftBig');
-}, { offset: 0 })
+}, { offset: 450 })
 
 $('.gifts .waypoint').waypoint(function(direction) {
 $(".gifts .inner_section").show().addClass('animated fadeInLeftBig');
-}, { offset: 0 })
+}, { offset: 450 })
 
 $('.when_where .waypoint').waypoint(function(direction) {
 $(".when_where .inner_section").show().addClass('animated fadeInLeftBig');
-}, { offset: 0 })
+}, { offset: 400 })
 
 $('.about_us .waypoint').waypoint(function(direction) {
 $(".about_us .inner_section").show().addClass('animated fadeInRightBig');
-}, { offset: 0 })
+}, { offset: 400 })
 
 $('.where_to_stay .waypoint').waypoint(function(direction) {
 $(".where_to_stay .inner_section").show().addClass('animated fadeInLeftBig');
-}, { offset: 0 })
+}, { offset: 350 })
 
 
 
